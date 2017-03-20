34d458f519c01ded2568994c4e78ffc25a84080b
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-03-19 22:05:53 -0400

    backend logic
    created the data structure for music stations
    created a method that sets the station and plays
    created a method to update the display

--- js/app.js
+++ js/app.js
@@ -1,5 +1,44 @@
 var $animation = document.getElementById("animation");
 var $animWrapper = document.getElementById("anim-wrapper");
+var $audio = document.getElementById("audio");
+
+var player = {
+    station: "",
+    paused: true,
+}
+
+
+var music = {
+   blues:  [
+        "blues",
+       {
+           info: "bar-legends",
+           url: "http://50.7.98.106:8719/;stream.nsv&type=mp3",
+           genre: "blues",
+       },
+       {
+           info: "blues-after-hours",
+           url: "http://46.23.68.170:8108/;stream.nsv&type=mp3",
+           genre: "blues",
+       }
+   ],
+}
+
+
+var bluestest = "http://50.7.98.106:8719/;stream.nsv&type=mp3";
+
+
+function play(newStation){
+    player.station = newStation;
+    player.paused = false;
+    $($audio).attr("src", newStation.url);
+    document.getElementById("station-text").innerHTML = newStation.info;
+    document.getElementById("genre-text").innerHTML = newStation.genre;
+}
+
+function stop(){
+    $($audio).attr("src", "");
+}
 
 
 function startStopper(){
@@ -8,16 +47,21 @@ function startStopper(){
             $($animation).removeClass("stopped");
             $($animation).addClass("spinning");
             $($animWrapper).addClass("pulsing");
+            play(music.blues[1]);
 
         } else {
             $($animation).removeClass("spinning");
             $($animWrapper).removeClass("pulsing");
             $($animation).addClass("stopped");
+            player.paused = true;
+            stop();
         }
 
 
     });
 }
+
+
 
 
 
