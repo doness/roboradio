82efbe37a08f39b172956d786456f8e8d769ab53
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-03-19 22:19:51 -0400

    updating the things

--- js/app.js
+++ js/app.js
@@ -10,16 +10,20 @@ var player = {
 
 var music = {
    blues:  [
-        "blues",
+        0,
        {
            info: "bar-legends",
            url: "http://50.7.98.106:8719/;stream.nsv&type=mp3",
            genre: "blues",
+           genreNumber: 0,
+           stationNumber: 1,
        },
        {
            info: "blues-after-hours",
            url: "http://46.23.68.170:8108/;stream.nsv&type=mp3",
            genre: "blues",
+           genreNumber: 0,
+           stationNumber: 1,
        }
    ],
 }
@@ -31,6 +35,8 @@ var bluestest = "http://50.7.98.106:8719/;stream.nsv&type=mp3";
 function play(newStation){
     player.station = newStation;
     player.paused = false;
+    player.genreNum = newStation.number;
+    player.stationNumber = newStation.stationNumber;
     $($audio).attr("src", newStation.url);
     document.getElementById("station-text").innerHTML = newStation.info;
     document.getElementById("genre-text").innerHTML = newStation.genre;
