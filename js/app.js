44d0e1be0acf8580a88c5b18b528465bd18d862f
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-03-27 15:38:53 -0400

    Design and Button Functions

--- js/app.js
+++ js/app.js
@@ -1,108 +1,218 @@
 var $animation = document.getElementById("animation");
 var $animWrapper = document.getElementById("anim-wrapper");
-var $audio = document.getElementById("audio");
+var button1 = document.getElementById("1");
+var button2 = document.getElementById("2");
 
-var player = {
-    station: "",
-    paused: true,
-    genreNumber: 0,
-    stationNumber: 0,
-}
-
+var totalGenres = 1;
 var musicarray = [
+
+    //GENRE BLUES
     [
         {
             info: "bar-legends",
             url: "http://50.7.98.106:8719/;stream.nsv&type=mp3",
             genre: "blues",
             genreNumber: 0,
-            stationNumber: 0,
+            stationNumber: 0
         },
         {
-            info: "blues-after-hours",
-            url: "http://46.23.68.170:8108/;stream.nsv&type=mp3",
+            info: "Deep Jams",
+            url: "http://74.50.122.103:9748/;stream.nsv&type=mp3",
             genre: "blues",
             genreNumber: 0,
-            stationNumber: 1,
+            stationNumber: 1
+        },
+        {
+            info: "Music City Roadhouse",
+            url: "http://stereo.wavestreamer.com:5022/;stream.nsv&type=mp3",
+            genre: "blues",
+            genreNumber: 0,
+            stationNumber: 2
         }
     ],
+
+    //GENRE BlUES TWO
     [
         {
-            info: "blues-after-hours",
-            url: "http://46.23.68.170:8108/;stream.nsv&type=mp3",
-            genre: "blues",
-            genreNumber: 0,
-            stationNumber: 1,
+            info: "big Blues Swing",
+            url: "http://www.bigblueswing.com:8002/;stream.nsv&type=mp3",
+            genre: "blues two",
+            genreNumber: 1,
+            stationNumber: 0
         },
         {
             info: "blues-after-hours",
             url: "http://46.23.68.170:8108/;stream.nsv&type=mp3",
-            genre: "blues",
-            genreNumber: 0,
-            stationNumber: 1,
+            genre: "blues two ",
+            genreNumber: 1,
+            stationNumber: 1
         }
-    ]
-]
+    ],
 
+    //EMPTY GENRE
+    //[
+    //    {
+    //
+    //    },
+    //    {
+    //
+    //    },
+    //    {
+    //
+    //    },
+    //    {
+    //
+    //    },
+    //    {
+    //
+    //    },
+    //    {
+    //
+    //    }
+    //]
+
+
+];
+
+
+var player = {
+    station: musicarray[0][0],
+    paused: true,
+    genreNumber: 0,
+    stationNumber: 0
+};
+
+var currentAudio = new Audio(player.station.url);
 
 var genres = [
     "blues", "rock", "pop", "electronic", "rap", "country",
-]
+];
 
 
-var bluestest =  new Audio('http://50.7.98.106:8719/;stream.nsv&type=mp3');
-function workaround(){
-    bluestest.play();
+function pause(){
+    currentAudio.pause();
+        $($animation).removeClass("spinning");
+        $($animWrapper).removeClass("pulsing");
+        $($animation).addClass("stopped");
+        player.paused = true;
 }
 
+function playMusic(){
 
-
-function play(newStation){
-    player.station = newStation;
+    currentAudio.src = player.station.url;
     player.paused = false;
-    player.genreNumber = newStation.number;
-    player.stationNumber = newStation.stationNumber;
-    $($audio).attr("src", newStation.url);
-    document.getElementById("station-text").innerHTML = newStation.info;
-    document.getElementById("genre-text").innerHTML = newStation.genre;
-}
 
-function stop(){
-    $($audio).attr("src", "");
+    $($animation).removeClass("stopped");
+    $($animation).addClass("spinning");
+    $($animWrapper).addClass("pulsing");
+    currentAudio.play();
 }
 
 
 function startStopper(){
-    $("#animation").click(function(){
-        if ($animation.className == "stopped") {
-            $($animation).removeClass("stopped");
-            $($animation).addClass("spinning");
-            $($animWrapper).addClass("pulsing");
-            play(musicarray[0][0]);
-
-        } else {
-            $($animation).removeClass("spinning");
-            $($animWrapper).removeClass("pulsing");
-            $($animation).addClass("stopped");
-            player.paused = true;
-            stop();
-        }
-
-
-    });
+    if(player.paused === false){
+        pause();
+    } else {
+        playMusic();
+    }
 }
 
 
+function setStation(newStation){
 
 
-$(document).ready(function(){
+    player.station = newStation;
+    player.stationNumber = newStation.stationNumber;
+    player.genreNumber = newStation.genreNumber;
+    document.getElementById("station-text").innerHTML = newStation.info;
+    document.getElementById("genre-text").innerHTML = newStation.genre;
+}
 
-    $("#1").click(function(){
-        workaround();
-    });
+$("#animation").click(function() {
+    setStation(player.station);
     startStopper();
 
+});
 
+$("#playPause").click(function(){
+    setStation(player.station);
+    startStopper();
+});
+
+$("#step-forward").click(function(){
+
+    if (player.stationNumber < musicarray[player.genreNumber].length -1) {
+        setStation(musicarray[player.genreNumber][(player.stationNumber +1)]);
+        playMusic();
+    } else {
+        setStation(musicarray[player.genreNumber][0]);
+        playMusic();
+    }
 
 });
+
+$("#genre-next").click(function(){
+    if (player.genreNumber < totalGenres){
+        setStation( musicarray[ (player.genreNumber + 1) ][0] );
+        playMusic();
+    } else {
+        setStation( musicarray[0][0] );
+        playMusic();
+    }
+});
+
+
+
+
+
+$("#1").click(function(){
+
+    setStation(musicarray[player.genreNumber][0]);
+    playMusic();
+});
+
+
+$("#2").click(function(){
+
+    setStation(musicarray[player.genreNumber][1]);
+    playMusic();
+});
+
+
+$("#3").click(function(){
+
+    setStation(musicarray[player.genreNumber][2]);
+    playMusic();
+});
+
+$("#4").click(function(){
+
+    setStation(musicarray[player.genreNumber][3]);
+    playMusic();
+});
+
+$("#5").click(function(){
+
+    setStation(musicarray[player.genreNumber][4]);
+    playMusic();
+});
+
+$("#6").click(function(){
+
+    setStation(musicarray[player.genreNumber][5]);
+    playMusic();
+});
+
+
+
+
+
+
+
+
+
+//$(document).ready(function(){
+//
+//
+//});
 
