5222c8045138b0a523411409553a2a63744bb278
Infinite_Loop() <mike.computerwiz@gmail.com>
2017-04-21 23:50:26 -0400

    volume test

--- js/app.js
+++ js/app.js
@@ -1223,3 +1223,35 @@ $(".station-triggers").click(function (e) {
     playMusic();
 });
 
+$("#volume-half").click(function(){
+    if (currentAudio.volume === 1){
+        currentAudio.volume = 0.25;
+    } else {
+        currentAudio.volume = 1;
+    }
+
+});
+//ID3 tagging
+
+//
+//document.querySelector('input[type="file"]').onchange = function(e) {
+//    var reader = new FileReader();
+//
+//    reader.onload = function(e) {
+//        var dv = new jDataView(this.result);
+//
+//        // "TAG" starts at byte -128 from EOF.
+//        // See http://en.wikipedia.org/wiki/ID3
+//        if (dv.getString(3, dv.byteLength - 128) == 'TAG') {
+//            var title = dv.getString(30, dv.tell());
+//            var artist = dv.getString(30, dv.tell());
+//            var album = dv.getString(30, dv.tell());
+//            var year = dv.getString(4, dv.tell());
+//        } else {
+//            // no ID3v1 data found.
+//        }
+//    };
+//
+//    reader.readAsArrayBuffer(this.files[0]);
+//};
+//
